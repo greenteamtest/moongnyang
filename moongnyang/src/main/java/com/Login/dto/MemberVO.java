1f97b6fb57397f4807dcd5ee8edaf08a4594d2c8
KimEunho <zhfldk014745@naver.com>
2021-07-19 13:33:11 +0900

    로그인 및 유저 정보 이메일 닉네임 비번 권한 딱 4가지

--- moongnyang/src/main/java/com/Login/dto/MemberVO.java
+++ moongnyang/src/main/java/com/Login/dto/MemberVO.java
@@ -1,20 +1,12 @@
 package com.Login.dto;
 
 public class MemberVO {
-<<<<<<< HEAD
-<<<<<<< HEAD
-	private String name;
-	private String userid;
-	private String pwd;
-	private String email;
-	private String phone;
-	private int admin;
-=======
+
 	private String email;
 	private String nickname;
 	private String pwd;
 	private int auth;
-	
+
 	public String getEmail() {
 		return email;
 	}
@@ -53,113 +45,4 @@ public class MemberVO {
 	public String toString() {
 		return "MemberVO [email=" + email + ", nickname=" + nickname + ", pwd=" + pwd + ", auth=" + auth + "]";
 	}
-
->>>>>>> branch 'master' of https://github.com/greenteamtest/moongnyang.git
-
-<<<<<<< HEAD
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getUserid() {
-		return userid;
-	}
-
-	public void setUserid(String userid) {
-		this.userid = userid;
-	}
-
-	public String getPwd() {
-		return pwd;
-	}
-
-	public void setPwd(String pwd) {
-		this.pwd = pwd;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public int getAdmin() {
-		return admin;
-	}
-
-	public void setAdmin(int admin) {
-		this.admin = admin;
-	}
-
-	@Override
-	public String toString() {
-		return "MemberVO [name=" + name + ", userid=" + userid + ", pwd=" + pwd + ", email=" + email + ", phone="
-				+ phone + ", admin=" + admin + "]";
-	}
-
-//	@Override
-//	public String toString() {
-//		return "MemberVo [name=" + name + ", user =" + userid + ", pwd=" + pwd + ", email=" + email + ", phone=" + phone
-//				+ ", admin=" + admin + "]";
-//	}
-=======
-	private String email;
-	private String nickname;
-	private String pwd;
-	private int auth;
-	
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getNickname() {
-		return nickname;
-	}
-
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
-	}
-
-	public String getPwd() {
-		return pwd;
-	}
-
-	public void setPwd(String pwd) {
-		this.pwd = pwd;
-	}
-
-	public int getAuth() {
-		return auth;
-	}
-
-	public void setAuth(int auth) {
-		this.auth = auth;
-	}
-
-	@Override
-	public String toString() {
-		return "MemberVO [email=" + email + ", nickname=" + nickname + ", pwd=" + pwd + ", auth=" + auth + "]";
-	}
-
->>>>>>> refs/heads/uno
-=======
->>>>>>> branch 'master' of https://github.com/greenteamtest/moongnyang.git
 }
