cd0be99b0bb2717fa1de578fcaa0baaba353b8a8
hodori33 <82789507+hodori33@users.noreply.github.com>
2021-07-19 14:08:13 +0900

    2:07

---
+++ moongnyang/src/main/java/dbconnect/MybatisSessionFactory.java
@@ -0,0 +1,32 @@
+package dbconnect;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class MybatisSessionFactory {
+
+	static SqlSessionFactory sessionFactory = null;
+
+	static {
+		String resource = "session/mybatis_setting.xml";
+		InputStream inputStream = null;
+		try {
+			inputStream = Resources.getResourceAsStream(resource);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		sessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+	}
+
+	public static SqlSession getSqlSession() {
+		SqlSession session = sessionFactory.openSession();
+
+		return session;
+	}
+
+}
---
+++ moongnyang/src/main/webapp/setting/setting.jsp
@@ -0,0 +1,7 @@
+
+<%@ page session="true" buffer="8kb" isThreadSafe="true" %>
+<%@ page info="" %>
+<%@ page errorPage="" %> <!-- exception : error page redirection url -->
+<%@ page isErrorPage="false" %> <!--error page : show error page of redirection   -->
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%> <!-- core JSTL -->
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %> <!-- format JSTL -->
