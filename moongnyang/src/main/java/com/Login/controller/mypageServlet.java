e9604909b6a202753b113acd239016f13edca097
KimEunho <zhfldk014745@naver.com>
2021-07-21 19:56:03 +0900

    슬슬 모델2 프로그래밍 기초 다지고, 마이바티스 사용할 준비중

---
+++ moongnyang/src/main/java/com/Login/controller/mypageServlet.java
@@ -0,0 +1,57 @@
+package com.Login.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.Login.controller.action.Action;
+
+/**
+ * Servlet implementation class mypageServlet
+ */
+@WebServlet("/mypageServlet")
+public class mypageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public mypageServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String command = request.getParameter("command");
+		System.out.println("mypageServlet에서 요청을 받음으 확인 : " + command);
+		ActionFactory af = ActionFactory.getInstance();
+		Action action = af.getAction(command);
+		if(action!=null) {
+			action.execute(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		doGet(request, response);
+
+	}
+
+}
---
+++ moongnyang/src/main/java/com/Login/dao/BoardDAO.java
@@ -0,0 +1,13 @@
+//package com.Login.dao;
+//
+//public class BoardDAO {
+//	public class BoardDAO {
+//		private BoardDAO() {
+//
+//		}
+//
+//		private static BoardDAO instance = new BoardDAO() {
+//			
+//		}
+//	} 공사중
+//}
