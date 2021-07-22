e7c5e3afb4b73ec602a283a748ca61d6614ab456
tmdwns0531 <tmdwns0531@naver.com>
2021-07-22 18:28:01 +0900

    21.07.22 - 다중 모달 까지 완료

--- moongnyang/src/main/webapp/health&edu/place/place_info.js
+++ moongnyang/src/main/webapp/health&edu/place/place_info.js
@@ -15,10 +15,19 @@ $('.card-img-top').click(function() {
 			rs[0].place.replace("\"", "");
 			rs[0].idx.replace("\"", "");
 
-			$(".place_img").attr('src', `health&edu/place/place_img/${rs[0].place}_${rs[0].idx}.png`);
+			const imgs = document.getElementsByClassName('d-block w-100');
+
+			for (let index = 0; index < imgs.length; index++) {
+				if (index == 0) {
+					imgs[index].src = `health&edu/place/place_img/${rs[0].place}_${rs[0].idx}.png`;
+				} else {
+					imgs[index].src = `health&edu/place/place_img/${rs[0].place}_${rs[0].idx}_${index}.png`;
+				}
+			}
+
 			$(".place").html(rs[0].place);
 			$(".address").html(rs[0].address);
-			$(".phon_num").html(rs[0].phone_num);
+			$(".phone_num").html(rs[0].phone_num);
 			$(".open_time").html(rs[0].open_time);
 			$(".introduce").html(rs[0].introduce);
 			$(".pet_kind").html(rs[0].pet_kind);
@@ -27,4 +36,33 @@ $('.card-img-top').click(function() {
 		}
 	})
 })
+
+
+$('.d-block.w-100').on('error', function() {
+	$(this).attr('src', 'health&edu/place/place_img/logo.png');
+})
+
+
+$('.title').click(function() {
+	$(this).next().toggle();
+})
+
+
+
+
+
+
+$('#btn1').click(function() {
+	$('#staticBackdrop').modal('show');
+})
+
+$('.ui.button').click(function() { // static 종료
+	$('#staticBackdrop').modal('hide');
+})
+
+
+$('.btn.btn-secondary').click(function() {
+	$('div').remove('.modal-backdrop.fade');
+})
+
 
