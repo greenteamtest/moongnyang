df630ed49dd0cc9d61d077455b79a30e52dc8234
KimEunho <zhfldk014745@naver.com>
2021-08-05 15:14:04 +0900

    읽음처리하면 바로바로 알림에서 숫자가 사라지게됨

--- moongnyang/src/main/java/com/Login/controller/userPageAnswerCheckAction.java
+++ moongnyang/src/main/java/com/Login/controller/userPageAnswerCheckAction.java
@@ -6,9 +6,11 @@ import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.Login.controller.action.Action;
 import com.Login.dao.BoardDAO;
+import com.Login.dao.MemberDAO;
 import com.Login.dto.boardVO;
 
 public class userPageAnswerCheckAction implements Action {
@@ -16,12 +18,15 @@ public class userPageAnswerCheckAction implements Action {
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-
+		HttpSession session = request.getSession();
 		String num = request.getParameter("num");
 		BoardDAO dao = BoardDAO.getInstance();
+		MemberDAO mdao = MemberDAO.getInstance();
 		dao.checkedAnswerBoard(num);
 		String email = request.getParameter("email");
+		int answerboard_check = mdao.count_answer(email);
 		List<boardVO> boardList = dao.selectUserAnswerBoards(email);
+		session.setAttribute("unchecked", answerboard_check);
 		request.setAttribute("boardList", boardList);
 		new userPageAction().execute(request, response);
 
