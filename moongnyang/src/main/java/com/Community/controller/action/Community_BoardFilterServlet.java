4667c5b688a02da807e58a464f5fc545a13079e4
junglegym999 <73105288+junglegym999@users.noreply.github.com>
2021-08-10 02:09:27 +0900

    게시판 리스트 필터기능 구현 완료 (필터 부트스트랩 적용 전 ,, 그래서 못생긴 모습..ㅜ)

--- moongnyang/src/main/java/com/Community/controller/action/Community_BoardFilterServlet.java
+++ moongnyang/src/main/java/com/Community/controller/action/Community_BoardFilterServlet.java
@@ -1,11 +1,18 @@
 package com.Community.controller.action;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.Community.dao.CommunityDAO;
+import com.Community.dto.CommunityVO;
 
 
 @WebServlet("/Filter.do")
@@ -26,8 +33,38 @@ public class Community_BoardFilterServlet extends HttpServlet {
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		request.setCharacterEncoding("UTF-8");
-		String animal_tag = request.getParameter("gender");
+		
+		//2개의 체크박스 그룹에서 고른 값 가져오기
+		//null값 처리 어떻게 하나...? 
+		String animal_tagArr[] = request.getParameterValues("animal_tag");
+		String board_tagArr[] = request.getParameterValues("board_tag");
+
+		
+		for (int i = 0; i < animal_tagArr.length; i++) {
+			animal_tagArr[i] = "animal_tag=" + animal_tagArr[i];
+		}
+		for (int i = 0; i < board_tagArr.length; i++) {
+			board_tagArr[i] = "board_tag=" + board_tagArr[i];
+		}
+		String query_animal = String.join(" or ", animal_tagArr);
+		String query_board = String.join(" or ", board_tagArr);
+		
+		//////////////////////////////////////////////////////////
+		
+	
+		
+		CommunityDAO cDAO = CommunityDAO.getInstance();
+		List<CommunityVO> boardList=cDAO.selectFilteredBoards(query_animal,query_board);
+
+		String url = "community/community_board_list.jsp";
+		
+		request.setAttribute("boardList", boardList);
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
+		dispatcher.forward(request, response);
+		
 		
 	}
 
